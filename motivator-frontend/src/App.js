2976ae1510767ece61ede0c947eb849c32637592
Gaurishankar S <gaurishankars@Gaurishankars-MacBook-Air.local>
2025-02-03 01:57:35 +0530

    Enable user-driven search

--- motivator-frontend/src/App.js
+++ motivator-frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>🔥 Your fun time</h1>
+      <h1>🔥 Your fun time, watch your favourite youtube videos</h1>
       
       <input
         type="text"
